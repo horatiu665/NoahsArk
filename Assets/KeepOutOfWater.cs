a4cca40add5946f11423290d50e8eb4650a195af
horatiu665 <croman12@student.aau.dk>
2014-05-05 15:56:45 +0200

    project setup, already some content
    terrain and some textures and player controller. also script that
    doesn't allow walking in water.

---
+++ Assets/KeepOutOfWater.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepOutOfWater : MonoBehaviour
+{
+	public float minHeight = -1f;
+	Vector3 prevPos;
+
+	void Start()
+	{
+		prevPos = transform.position;
+
+	}
+
+	// at each physics step, make sure the player does not go below a certain depth. if he does, move him to his previous position.
+	void FixedUpdate()
+	{
+		if (transform.position.y < minHeight) {
+			if (transform.position.y < prevPos.y) {
+				transform.position = prevPos;
+			}
+		}
+
+
+
+		prevPos = transform.position;
+	}
+}
