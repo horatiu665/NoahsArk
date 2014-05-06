29c2a39a6100a76d92f3d977ccb30f35760b3ae3
JohanStricker <jdestr12@student.aau.dk>
2014-05-06 13:19:10 +0200

    wiggle scribt complete

---
+++ Assets/scripts/Wiggle.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wiggle : MonoBehaviour {
+	public bool S_tart;
+	[Range(0,1)]
+	public float speed;
+	[Range(0,1)]
+	public float Xangle;
+	[Range(0,1)]
+	public float Yangle;
+	[Range(0,1)]
+	public float Zangle;
+	private float x,y,z;
+
+
+	// Use this for initialization
+	void Start () {
+		S_tart = false;
+		x = 1;
+		y = 1;
+		z = 1;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//work in progress
+		if(transform.rotation.x>Xangle){
+			x = speed*-1;
+		}else if(transform.rotation.x<-Xangle){
+			x = speed;
+		}
+		if(transform.rotation.y>Yangle){
+			y = speed*-1;
+		}else if(transform.rotation.y<-Yangle){
+			y = speed;
+		}
+		if(transform.rotation.z>Zangle){
+			z = speed*-1;
+		}else if(transform.rotation.z<-Zangle){
+			z = speed;
+		}
+		if(S_tart){
+			if(Xangle>0&&Yangle>0&&Zangle>0){
+				transform.Rotate(new Vector3(x,y,z));
+
+			}else if(Xangle==0&&Yangle>0&&Zangle>0){
+				transform.Rotate(new Vector3(0,y,z));
+
+			}else if(Xangle>0&&Yangle==0&&Zangle>0){
+				transform.Rotate(new Vector3(x,0,z));
+				
+			}else if(Xangle==0&&Yangle>0&&Zangle==0){
+				transform.Rotate(new Vector3(x,y,0));
+
+			}else if(Xangle>0&&Yangle==0&&Zangle==0){
+				transform.Rotate(new Vector3(x,0,0));
+				
+			}else if(Xangle==0&&Yangle>0&&Zangle==0){
+				transform.Rotate(new Vector3(0,y,0));
+
+			}else if(Xangle>0&&Yangle>0&&Zangle==0){
+				transform.Rotate(new Vector3(0,0,z));
+			}
+		}
+
+
+	}
+}
