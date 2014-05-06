6a6519650b57d9270630874aefdf630603ade653
JohanStricker <jdestr12@student.aau.dk>
2014-05-06 16:10:53 +0200

    Wiggle script fixed

--- Assets/scripts/Wiggle.cs
+++ Assets/scripts/Wiggle.cs
@@ -25,22 +25,25 @@ public class Wiggle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//work in progress
+		//angle for when to turn back for X
 		if(transform.rotation.x>Xangle){
 			x = speed*-1;
 		}else if(transform.rotation.x<-Xangle){
 			x = speed;
 		}
+		//angle for when to turn back for Z
 		if(transform.rotation.y>Yangle){
 			y = speed*-1;
 		}else if(transform.rotation.y<-Yangle){
 			y = speed;
 		}
+		//angle for when to turn back for Z
 		if(transform.rotation.z>Zangle){
 			z = speed*-1;
 		}else if(transform.rotation.z<-Zangle){
 			z = speed;
 		}
+
 		if(S_tart){
 			if(Xangle>0&&Yangle>0&&Zangle>0){
 				transform.Rotate(new Vector3(x,y,z));
@@ -51,7 +54,7 @@ public class Wiggle : MonoBehaviour {
 			}else if(Xangle>0&&Yangle==0&&Zangle>0){
 				transform.Rotate(new Vector3(x,0,z));
 				
-			}else if(Xangle==0&&Yangle>0&&Zangle==0){
+			}else if(Xangle>0&&Yangle>0&&Zangle==0){
 				transform.Rotate(new Vector3(x,y,0));
 
 			}else if(Xangle>0&&Yangle==0&&Zangle==0){
@@ -60,7 +63,7 @@ public class Wiggle : MonoBehaviour {
 			}else if(Xangle==0&&Yangle>0&&Zangle==0){
 				transform.Rotate(new Vector3(0,y,0));
 
-			}else if(Xangle>0&&Yangle>0&&Zangle==0){
+			}else if(Xangle==0&&Yangle==0&&Zangle>0){
 				transform.Rotate(new Vector3(0,0,z));
 			}
 		}
